4f4e01512e344b8e7c1f1aef4a5d613bc8a550a9
matias salvatore <[EMAIL]>
2023-01-20 10:42:06 +0100

    routes

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const UserController = require("../controllers/UserController");
-const { authentication } = require("../middlewares/authentication");
+const { authentication, isAdmin } = require("../middlewares/authentication");
 const upload = require("../middlewares/upload");
 
 router.post("/register", upload.single("imageUser"), UserController.register);
